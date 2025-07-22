2ec8417f199254c8797a5a941c10308f13a2af5e
Harish <shreeharish2610@gmail.com>
2025-07-22 20:22:05 +0530

    error correction

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -64,7 +64,7 @@ export default function ModernKarthikeyanPortfolio() {
     }
   }, [currentIndex, achievements.length]);
 
-  const goToSlide = (index) => {
+  const goToSlide = (index:any) => {
     setIsTransitioning(true);
     setCurrentIndex(index + 2);
   };
